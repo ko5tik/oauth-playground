792236b6761fe999fe6343552c23d38f6b9fbf84
ko5tik <konstantin.pribluda@gmail.com>
2021-02-19 11:52:06 +0200

    created docker build for server

--- auth-server/main.go
+++ auth-server/main.go
@@ -5,18 +5,21 @@ import (
 	"github.com/ko5tik/oauth-playground/server"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 )
 
 func main() {
 
-	portPtr := flag.Int("port", 3846, "listener port for server,  default 3846")
+	port := "3846"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
 
 	flag.Parse()
 
 	server.RegisterHandlers()
 
 	// fire up webserver on designated port passed via command line
-	log.Println("auth server is listening on port ", *portPtr)
-	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*portPtr), nil))
+	log.Println("auth server is listening on port ", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
