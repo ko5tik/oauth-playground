73f06ddf0832c1e98bd68969fb1e81a1e0162091
ko5tik <konstantin.pribluda@gmail.com>
2021-02-26 12:37:45 +0200

    loaded key works

--- client-credentials-server/server/compose.go
+++ client-credentials-server/server/compose.go
@@ -1,12 +1,15 @@
 package server
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"github.com/ory/fosite/compose"
 	"github.com/ory/fosite/handler/oauth2"
 	"github.com/ory/fosite/storage"
 	"github.com/ory/fosite/token/jwt"
+	"io/ioutil"
+	"log"
 	"time"
 )
 
@@ -58,7 +61,7 @@ var (
 	secret = []byte("some-cool-secret-that-is-32bytes")
 
 	// privateKey is used to sign JWT tokens. The default strategy uses RS256 (RSA Signature with SHA-256)
-	privateKey, _ = rsa.GenerateKey(rand.Reader, 2048)
+	privateKey, _ = readKey() //rsa.GenerateKey(rand.Reader, 2048)
 )
 
 // Build a fosite instance with all OAuth2 and OpenID Connect handlers enabled, plugging in our configurations as specified above.
@@ -78,3 +81,38 @@ var fosite = compose.Compose(
 
 	compose.OAuth2ClientCredentialsGrantFactory,
 )
+
+//read key from well known location
+func readKey() (*rsa.PrivateKey, error) {
+	priv, err := ioutil.ReadFile("server.pem")
+	if err != nil {
+		log.Fatal("unable to load private key file")
+	}
+
+	privPem, _ := pem.Decode(priv)
+	var privPemBytes []byte
+	if privPem.Type != "RSA PRIVATE KEY" {
+		log.Fatal("not RSA key file")
+	}
+
+	privPemBytes, err = x509.DecryptPEMBlock(privPem, []byte("foobar"))
+	if err != nil {
+		log.Fatal("unable to decrypt privatet ekey file")
+	}
+
+	var parsedKey interface{}
+	if parsedKey, err = x509.ParsePKCS1PrivateKey(privPemBytes); err != nil {
+		if parsedKey, err = x509.ParsePKCS8PrivateKey(privPemBytes); err != nil { // note this returns type `interface{}`
+			log.Fatal("unable to parse private key")
+		}
+	}
+
+	var privateKey *rsa.PrivateKey
+	var ok bool
+	privateKey, ok = parsedKey.(*rsa.PrivateKey)
+	if !ok {
+		log.Fatal("cast fialed")
+	}
+
+	return privateKey, nil
+}
